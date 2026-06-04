d3c7a78f21876de39ec1eda92f93e7881eea1f9b
tanishqnikose9302 <99585051+tanishqnikose9302@users.noreply.github.com>
2026-06-04 16:37:22 +0530

    Create loader.py

---
+++ ingestion/loader.py
@@ -0,0 +1,8 @@
+from PyPDF2 import PdfReader
+
+def load_pdf(file_path):
+    reader = PdfReader(file_path)
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text()
+    return text
