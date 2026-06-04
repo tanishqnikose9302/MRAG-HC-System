e97e12b8b20a53b28ac20d8ad20f9bbb64a56244
tanishqnikose9302 <99585051+tanishqnikose9302@users.noreply.github.com>
2026-06-04 16:34:56 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,5 @@
-import uvicorn
+import streamlit as st
+from app.ui_streamlit import *
 
 if __name__ == "__main__":
-    uvicorn.run(
-        "app.main:app",
-        host="0.0.0.0",
-        port=8000,
-        reload=True
-    )
+    st.run()
